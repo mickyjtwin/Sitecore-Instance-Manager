61a1363e3506a350faf9c2d87c2adaf6c43d31a8
dsolovay <dan.solovay@velir.com>
2016-06-26 10:58:03 -0400

    Add CreateSolrCore command and tests

---
+++ src/SIM.Pipelines/Install/Modules/CreateSolrCore.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.CodeDom;
+using System.IO;
+using System.Net;
+using System.Xml;
+using SIM.Instances;
+using SIM.Pipelines.InstallModules;
+using SIM.Products;
+
+namespace SIM.Pipelines.Install.Modules
+{
+  public class CreateSolrCore : IPackageInstallActions
+  {
+    /// <summary>
+    /// Wrap WebRequestHelper for unit testing.
+    /// </summary>
+    /// <param name="url"></param>
+    /// <returns></returns>
+    public virtual HttpWebResponse RequestAndGetResponse(string url)
+    {
+      return WebRequestHelper.RequestAndGetResponse(url);
+    }
+
+    public virtual void ExecuteSystemCommand(string cmd)
+    {
+      throw new NotImplementedException();
+    }
+
+
+    public void Execute(Instance instance, Product module)
+    {
+      XmlDocument config = instance.GetShowconfig();
+      string url = config.SelectSingleNode("/sitecore/settings/setting[@name='ContentSearch.Solr.ServiceBaseAddress']").Attributes["value"].Value;
+
+      XmlNodeList solrIndexes =
+          config.SelectNodes(
+              "/sitecore/contentSearch/configuration/indexes/index[@type='Sitecore.ContentSearch.SolrProvider.SolrSearchIndex, Sitecore.ContentSearch.SolrProvider']");
+
+
+      string collection1Path = GetCollection1Path(url);
+      
+
+      foreach (var node in solrIndexes)
+      {
+        var element = node as XmlElement;
+        if (element == null) continue;
+        string id = element.Attributes["id"].InnerText;
+        var coreElement = element.SelectSingleNode("param[@desc='core']") as XmlElement;
+       
+        if (coreElement == null) continue;
+
+        string coreName = coreElement.InnerText.Replace("$(id)", id);
+
+        Log.Info("Core found:"+coreName, this);
+
+        string newCorePath = collection1Path.Replace("collection1", coreName);
+
+        CreateCoreDirectory(collection1Path, newCorePath);
+
+        CreateCorePropertiesFile(coreName, newCorePath);
+
+        CallSolrCreateCoreAPI(url, coreName);
+        
+
+         
+      }
+
+
+    }
+
+    private void CallSolrCreateCoreAPI(string url, string coreName)
+    {
+      HttpWebResponse response = this.RequestAndGetResponse(string.Format(
+        "{0}/admin/collections?action=CREATE&name={1}", url, coreName));
+    }
+
+    private void CreateCorePropertiesFile(string coreName, string newCorePath)
+    {
+      this.ExecuteSystemCommand(string.Format("echo name={0} > {1}core.properties",coreName,newCorePath));
+    }
+
+    private void CreateCoreDirectory(string collection1Path, string newCorePath)
+    {
+      this.ExecuteSystemCommand(string.Format("robocopy /e {0} {1}",
+        collection1Path,
+        newCorePath));
+    }
+
+    private string GetCollection1Path(string url)
+    {
+      var response = this.RequestAndGetResponse(string.Format(
+        "{0}/solr/admin/cores", url));
+
+      
+      var doc = new XmlDocument();
+      doc.Load(response.GetResponseStream());
+
+      XmlNode collection1Node = doc.SelectSingleNode("/response/lst[@name='STATUS']/lst[@name='collection1']");
+      if (collection1Node == null) throw new ApplicationException("collection1 not found");
+
+      return collection1Node.SelectSingleNode("str[@name='instanceDir']").InnerText;
+
+
+    }
+  }
+}
---
+++ src/SIM.Tests/Pipelines/CreateSolrCoreTests.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Xml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using SIM.Instances;
+using SIM.Pipelines;
+using SIM.Pipelines.Install.Modules;
+using SIM.Products;
+
+namespace SIM.Tests.Pipelines
+{
+  [TestClass]
+  public class CreateSolrCoreTests
+  {
+    private CreateSolrCore _sut;
+    private Instance _instance;
+    private Product _module;
+
+    [TestInitialize]
+    public void SetUp()
+    {
+      _sut = Substitute.For<CreateSolrCore>();
+      _instance = Substitute.For<Instance>();
+      _module = Substitute.For<Product>();
+      XmlDocument doc = new XmlDocument();
+      doc.LoadXml(GetConfigXml("SOME_URL", "SOME_CORE_NAME"));
+      _instance.GetShowconfig().Returns(doc);
+
+    }
+
+    private void Arrange()
+    {
+      ArrangeGetCores("<lst name='collection1'>"+
+                      "<str name='instanceDir'>c:\\some\\path\\collection1\\</str>"+
+                      "</lst>");
+    }
+
+    private void Act()
+    {
+      _sut.Execute(_instance, _module);
+    }
+
+    [TestMethod]
+    public void ShouldGetCores()
+    {
+      Arrange();
+
+      Act();
+
+      _sut.Received().RequestAndGetResponse("SOME_URL/solr/admin/cores");
+    }
+
+    [TestMethod, ExpectedException(typeof(ApplicationException))]
+    public void ShouldThrowIfNoCollection1()
+    {
+      ArrangeGetCores("");
+
+      Act();
+    }
+
+    [TestMethod]
+    public void ShouldCopyCollection1InstanceDirToNewCorePath()
+    {
+      Arrange();
+
+      Act();
+
+      _sut.Received().ExecuteSystemCommand("robocopy /e c:\\some\\path\\collection1\\ c:\\some\\path\\SOME_CORE_NAME\\");
+    }
+
+    [TestMethod]
+    public void ShouldCopyNewCorePropertiesFile()
+    {
+      Arrange();
+
+      Act();
+
+      _sut.Received().ExecuteSystemCommand("echo name=SOME_CORE_NAME > c:\\some\\path\\SOME_CORE_NAME\\core.properties");
+    }
+
+
+    [TestMethod]
+    public void ShouldCreateCore()
+    {
+      Arrange();
+
+      _sut.Execute(_instance,_module);
+
+
+      _sut.Received().RequestAndGetResponse("SOME_URL/admin/collections?action=CREATE&name=SOME_CORE_NAME");
+
+    }
+
+    // TODO Copy sitecore optimized Schema.xml
+    // TODO Wire ExecuteSystemCommand to proper SIM utility
+
+    private string GetConfigXml(string someUrl, string someCoreName)
+    {
+      return "<sitecore>" +
+             "<settings>" +
+             string.Format("<setting name='ContentSearch.Solr.ServiceBaseAddress' value='{0}' />", someUrl) +
+             "</settings>" +
+             "<contentSearch>" +
+             "<configuration>" +
+             "<indexes>" +
+             "<index  type='Sitecore.ContentSearch.SolrProvider.SolrSearchIndex, Sitecore.ContentSearch.SolrProvider' id='id1'>" +
+    
+             string.Format("<param desc='core' id='$(id)'>{0}</param>", someCoreName) +
+             "</index></indexes></configuration></contentSearch></sitecore>";
+    }
+
+    private void ArrangeGetCores(string coreInfo)
+    {
+      HttpWebResponse response = Substitute.For<HttpWebResponse>();
+      string returnValue = string.Format("<response><lst name='STATUS' >{0}</lst></response>", coreInfo);
+      var bytes = UTF8Encoding.UTF8.GetBytes(returnValue);
+      response.GetResponseStream().Returns(new MemoryStream(bytes));
+      _sut.RequestAndGetResponse("SOME_URL/solr/admin/cores").Returns(response);
+    }
+  }
+}
